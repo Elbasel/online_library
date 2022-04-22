39fd5e1912fd1eb3bedfb352b222329c97001f9e
elbasel <abdelrahman.elbasel@gmail.com>
2022-04-23 01:27:24 +0200

    added form effects

--- script.js
+++ script.js
@@ -43,12 +43,66 @@ function addBookToDom(book) {
 }
 
 
+function toggleForm() {
+    for (const elem of domElements.backgroundContainers) {
+        if (elem.style.display === 'none' || elem.style.display === '') {
+            elem.style.display = 'flex';
+            elem.style.opacity = 1;
+
+        }
+        else {
+            elem.style.opacity = '0';
+            setTimeout(() => elem.style.display = 'none', 650);
+        }
+    }
+}
+
+function getCurrentInputBook() {
+    const bookTitle = domElements.titleInput.value;
+    const bookAuthor = domElements.authorInput.value;
+    const bookNumOfPages = domElements.numOfPagesInput.value;
+
+
+    return new Book(getNewId(), bookTitle, bookAuthor, +bookNumOfPages)
+}
+
+function getNewId() {
+    return 0
+}
+
+function validateFormInputs() {
+    return true
+}
+
+function formSubmitEventListerner(e) {
+    if (validateFormInputs()) {
+        addBookToDom(getCurrentInputBook())
+        toggleForm()
+    }
+}
+
 const domElements = {
-    main: document.querySelector('main')
-} 
+    main: document.querySelector('main'),
+    addBookButton: document.querySelector('#add-book-button'),
+
+
+    form: document.querySelector('form'),
+    backgroundContainers: document.querySelectorAll('.container'),
+
+    titleInput: document.querySelector('#title-input'),
+    authorInput: document.querySelector('#author-input'),
+    numOfPagesInput: document.querySelector('#numOfPages-input'),
+
+    formSubmitButton: document.querySelector('#form-submit-button'),
+    formClosebutton: document.querySelector('#close-form-button')
+
+}
 
 
 
 
-
-
+domElements.addBookButton.addEventListener('click', () => toggleForm())
+domElements.formClosebutton.addEventListener('click', () => toggleForm())
+domElements.formClosebutton.addEventListener('click', e => e.preventDefault())
+domElements.form.addEventListener('submit', e => e.preventDefault())
+domElements.form.addEventListener('submit', e => formSubmitEventListerner(e))
